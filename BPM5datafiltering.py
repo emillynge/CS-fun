e905fad69d805cb99faca6309ad300139f9d59ad
Emil Sauer Lynge <emillynge24@gmail.com>
2015-10-18 00:33:53 +0200

    Download data on the fly

--- BPM5datafiltering.py
+++ BPM5datafiltering.py
@@ -1,1 +1,13 @@
 __author__ = 'emil'
+import requests
+import numpy as np
+data_url = "https://dl.dropboxusercontent.com/u/2640195/BPM5_data.txt"
+
+def url2mat(url):
+    r = requests.get(url)
+    mat = np.fromiter((float(element) for line in r.text.split('\n') if '\t' in line for element in line.split('\t')),
+                      float)
+    return np.matrix(mat.reshape((len(mat)/3, 3)))
+
+raw_data = url2mat(data_url)
+print(raw_data)
