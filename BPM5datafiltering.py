cdb6ad2923e380e4c8a2fb51ec915715630deb96
Emil Sauer Lynge <emillynge24@gmail.com>
2015-10-18 00:43:20 +0200

    Save/load switch

--- BPM5datafiltering.py
+++ BPM5datafiltering.py
@@ -1,13 +1,23 @@
 __author__ = 'emil'
 import requests
 import numpy as np
-data_url = "https://dl.dropboxusercontent.com/u/2640195/BPM5_data.txt"
+from matplotlib import pyplot as plt
 
+redownload_data = False
+
+data_url = "https://dl.dropboxusercontent.com/u/2640195/BPM5_data.txt"
 def url2mat(url):
     r = requests.get(url)
     mat = np.fromiter((float(element) for line in r.text.split('\n') if '\t' in line for element in line.split('\t')),
                       float)
     return np.matrix(mat.reshape((len(mat)/3, 3)))
+if redownload_data:
+    raw_data = url2mat(data_url)
+    np.save('rawdat.npy', raw_data)
+else:
+    raw_data = np.load('rawdat.npy')
 
-raw_data = url2mat(data_url)
+
 print(raw_data)
+
+
